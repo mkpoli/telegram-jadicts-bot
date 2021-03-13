87fe28733e97ebec5bbeec24cdb92f220786c40d
mkpoli <snowwolf1001@gmail.com>
2021-03-13 15:56:01 +0900

    Fix katakana bug

--- kana_convert.py
+++ kana_convert.py
@@ -2,13 +2,19 @@ import pykakasi
 import regex
 import jaconv
 
+from loguru import logger
+
 kks = pykakasi.kakasi()
 
 KANJI_PATTERN = regex.compile(r"(\p{Han}+)")
 def okurigana(orig, hira):
     # e.g. お悔やみ申し上げる
-    new_pattern = KANJI_PATTERN.sub(r"(\p{Hiragana}+)", orig) # お(\p{Hiragana}+)やみ(\p{Hiragana}+)し(\p{Hiragana}+)げる
-    mgroups = regex.match(new_pattern, hira).groups() # く, もう, あ
+    new_pattern = KANJI_PATTERN.sub(r"([\p{Hiragana}\p{Katakana}]+)", jaconv.kata2hira(orig)) # お(\p{Hiragana}+)やみ(\p{Hiragana}+)し(\p{Hiragana}+)げる
+    try:
+        mgroups = regex.match(new_pattern, hira).groups() # く, もう, あ
+    except AttributeError as e:
+        logger.error(f"No matched groups. orig={orig}, hira={hira} | new_pattern='{new_pattern}'")
+        raise e
     filling = KANJI_PATTERN.sub(r"\1{}", orig) # お悔{}やみ申{}し上{}げる
     return filling.format(*[f"（{x}）" for x in mgroups]) # お悔（く）やみ申（もう）し上（あ）げる
 

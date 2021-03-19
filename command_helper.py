a9aa75a957d52f839876ab3b12133b44a0b67afb
mkpoli <snowwolf1001@gmail.com>
2021-03-20 04:16:00 +0900

    Delete command help after 10 secs

--- command_helper.py
+++ command_helper.py
@@ -1,7 +1,8 @@
 from distutils.util import strtobool
+from loguru import logger
 from telegram import Update, Message
-from telegram.ext import CallbackContext
-from typing import Callable, Optional, Sequence, Tuple
+from telegram.ext import CallbackContext, Dispatcher
+from typing import Callable, Optional, Sequence, Tuple, TypeVar
 from logging_utils import bind_logger
 
 class BadUsage(ValueError):
@@ -40,7 +41,7 @@ class Command:
             try:
                 args = parse_command(self.parameters, self.description, update, self.last_ignore_space)
             except BadUsage as e:
-                command_usage(self.name, self.parameters, self.description, update)
+                command_usage(self.name, self.parameters, self.description, update.effective_message)
                 bind_logger(update).debug(f'BadUsage: {e}')
                 return
             self.handler(update, context, self, args)
@@ -92,15 +93,52 @@ def parse_command(parameters: Sequence[Parameter], description: str, update: Upd
 
     return result
 
-def command_usage(command: str, parameters: Sequence[Parameter], description: str, update: Update) -> None: 
+import time
+
+def _delete_after(delay: int, message: Message):
+    """Delete message after specified seconds.
+
+    Args:
+        delay (int): Delay in secs
+        message (Message): Message to delete
+    """
+    time.sleep(delay)
+    message.delete()
+    logger.debug(f'Message {message.message_id} in {message.chat.type} chat {message.chat.id} deleted:\n{message.text}')
+    return
+
+RT = TypeVar('RT')
+def delete_after(delay: int) -> Callable[[Callable[..., Message]], Callable[..., None]]:
+    def decorator(func: Callable[..., Message]) -> Callable[..., None]:
+        def wrapper(*args, **kwargs) -> None:
+            sent_message = func(*args, **kwargs)
+            if not isinstance(sent_message, Message):
+                raise TypeError('Message sender is not returning sent message')
+            dispatcher = Dispatcher.get_instance()
+            dispatcher.run_async(_delete_after, delay, sent_message)
+        return wrapper
+    return decorator
+            
+@delete_after(10)
+def command_usage(command: str, parameters: list[Parameter], description: str, reply_to: Message) -> Message: 
+    """Reply to user when a bad command usage is found.
+
+    Args:
+        command (str): Command Name
+        parameters (Sequence[Parameter]): Parameters of Command
+        description (str): Description of Command
+        reply_to (Message): User Message of Bad Usage
+    """
     command_repr = f"/{command} {' '.join(f'⟨{param.name}⟩' for param in parameters)}"
     if parameters:
         max_length = max(len(param.name) for param in parameters)
         paramtr_desc = "\n".join(f"    <code>{param.name.rjust(max_length)}</code> - {param.desc} " for param in parameters)
     else:
         paramtr_desc = "〈引数なし〉"
-    update.effective_message.reply_text(
+    sent_message = reply_to.reply_text(
         f"<b>コマンドヘルプ</b>\n<code>{ command_repr }</code>\n\n{ description }\n{ paramtr_desc }")
+    
+    return sent_message
 
 MAX_MESSAGE_TXT_LENGTH = 4096
 RESERVE_SPACE = 10

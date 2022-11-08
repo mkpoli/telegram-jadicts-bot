5369ed4b68ea3253d6c21c23f6642c0e6edb9b78
mkpoli <git@mkpo.li>
2022-11-08 21:49:54 +0900

    Remove python 3.9 syntax

--- _env.py
+++ _env.py
@@ -4,11 +4,12 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
 
-if not (TOKEN := os.environ.get('TELEGRAM_BOT_TOKEN')):
-    with open(BASE_DIR / 'TOKEN') as f:
+TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+if not TOKEN:
+    with open(BASE_DIR / "TOKEN") as f:
         TOKEN = f.read().strip()
 
 if not TOKEN:
     raise KeyError("No TOKEN found!")
 
-PRODUCTION_MODE = bool(os.environ.get('TELEGRAM_BOT_PRODUCTION', default=False))
+PRODUCTION_MODE = bool(os.environ.get("TELEGRAM_BOT_PRODUCTION", default=False))
